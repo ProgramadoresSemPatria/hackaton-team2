639b6c94b3da823244efd87741fc66c7040521fd
João Figueiredo <joao.ggwp@outlook.com>
2025-03-26 20:17:07 -0300

    feat: initial nest setup

---
+++ backend/src/core/user/application/use-cases/create-user/create-user.use-case.ts
@@ -0,0 +1,14 @@
+import { IUserRepository } from "../../../domain/contracts/repository/user.repository";
+import {
+  ICreateUserInput,
+  ICreateUserUseCase,
+} from "../../../domain/contracts/use-cases/create-user";
+import { User } from "../../../domain/user.aggregate";
+
+export class CreateUserUseCase implements ICreateUserUseCase {
+  constructor(private readonly userRepository: IUserRepository) {}
+  async execute({ name, email, password }: ICreateUserInput): Promise<void> {
+    const entity = User.create({ name, email, password });
+    await this.userRepository.create(entity);
+  }
+}
