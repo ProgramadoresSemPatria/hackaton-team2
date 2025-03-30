bf2a724468d4a79b24407ee2d84a3017c0a346c0
Cauê Alves <cauealvesb4@gmail.com>
2025-03-29 21:01:06 -0300

    feat: create new interview modal

--- front-end/src/view/components/Modal.tsx
+++ front-end/src/view/components/Modal.tsx
@@ -7,7 +7,7 @@ export default function Modal({ children, isOpen }: ModalProps) {
 
   return (
     <div className="bg-black/50 p-8 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 flex justify-center items-center w-full h-full">
-      <div className="py-6 px-8 rounded-lg bg-primary-color-darker w-full max-w-lg">
+      <div className="py-6 px-8 rounded-lg bg-primary-color-darker w-full max-w-lg  max-h-135 overflow-y-scroll">
         {children}
       </div>
     </div>
