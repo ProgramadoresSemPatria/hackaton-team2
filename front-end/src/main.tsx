ae8b0d7ef49270f6dcb0d864eb2eec2fa2d06f26
Cauê Alves <cauealvesb4@gmail.com>
2025-03-30 12:01:43 -0300

    style: making the transitions for the modal to appear and disappear

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AppRoutes } from "./routes.tsx";
 import "./index.css";
-import Dashboard from "./view/pages/dashboard/Dashboard.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		{/* <AppRoutes />  */}
-		<Dashboard />
+		<AppRoutes />
 	</StrictMode>
 );
