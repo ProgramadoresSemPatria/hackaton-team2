5291854e9d021c16e2ec5afe5df079990e7c780d
Cauê Alves <cauealvesb4@gmail.com>
2025-03-30 14:29:59 -0300

    chore: calling the BigInput component for the company feedback modal

--- front-end/src/view/pages/dashboard/feedback-company/ModalFeedbackCompany.tsx
+++ front-end/src/view/pages/dashboard/feedback-company/ModalFeedbackCompany.tsx
@@ -1,3 +1,4 @@
+import BigInput from "../../../components/BigInput";
 import Button from "../../../components/Button";
 import Modal from "../../../components/Modal";
 
@@ -11,12 +12,7 @@ export default function ModalFeedbackCompany({ isOpen, onClose }: ModalFeedbackC
         <Modal isOpen={isOpen}>
             <section className="text-color-font flex flex-col gap-3">
                 <h2 className="font-semibold text-center sm:text-left mb-2">Qual foi o feedback da empresa?</h2>
-
-                {/* OBS: exclude this code when creating the "BigInput" component */}
-                <textarea aria-label="Campo para inserir feedback da empresa"
-                          autoFocus 
-                          rows={6} 
-                          className="w-full resize-none bg-bg-input rounded-lg p-4 outline-none"></textarea>
+                <BigInput requiredInput={false}/>
 
                 <footer className="flex flex-col-reverse items-center sm:grid sm:grid-cols-2 gap-3 mt-3">
                     <a onClick={onClose} href="#" className="text-center hover:text-place-color transition-colors duration-150 ease-linear p-3">Pular</a>
