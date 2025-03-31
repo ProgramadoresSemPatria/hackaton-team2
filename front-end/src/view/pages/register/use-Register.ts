6ca6f9a7835762cdd0a02854ad4f7a434d8b84ac
michelzaum <michel.oliveira999@hotmail.com>
2025-03-31 20:41:16 -0300

    feat: integrate user register feature with back-end

--- front-end/src/view/pages/register/use-Register.ts
+++ front-end/src/view/pages/register/use-Register.ts
@@ -3,54 +3,55 @@ import { useNavigate } from "react-router";
 import { api } from "../../../api/baseRequest";
 
 enum STATUS_CODE {
-    OK = 200
+  OK = 200,
+  CREATED = 201,
 }
 
 export default function useRegister() {
-    const navigate = useNavigate()
-    const nameRef = useRef<HTMLInputElement>(null)
-    const emailRef = useRef<HTMLInputElement>(null)
-    const passwordRef = useRef<HTMLInputElement>(null)
-    const confirmPasswordRef = useRef<HTMLInputElement>(null)
+  const navigate = useNavigate();
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const confirmPasswordRef = useRef<HTMLInputElement>(null);
 
-    async function handleRegister() {
-        const name = nameRef.current?.value
-        const email = emailRef.current?.value
-        const password = passwordRef.current?.value
-        const confirmPassword = confirmPasswordRef.current?.value
+  async function handleRegister(): Promise<void> {
+    const name = nameRef.current?.value;
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    const confirmPassword = confirmPasswordRef.current?.value;
 
-        try {
-
-            if(password != confirmPassword ) {
-                alert("A senha não é a mesma")
-                return
-            }
-            console.log("oi1")
-
-            const response = await api.post("user", {
-                name, 
-                email,
-                password
-            })
-            
-            if (response.status === STATUS_CODE.OK) {
-                localStorage.setItem("token", response.data.token);
-                navigate("/dashboard");
-                console.log("oi2")
-                return;
-              }
-
-              alert("Erro no registro. Por favor, tente novamente");
-        } catch (error) {
-            console.error("Erro ao tentar fazer o registro: ", error); 
-        }
+    if (password != confirmPassword) {
+      alert("A senha não é a mesma");
+      return;
     }
 
-    return {
-        nameRef,
-        emailRef,
-        passwordRef,
-        confirmPasswordRef,
-        handleRegister
+    try {
+      const response = await api.post("user", {
+        name,
+        email,
+        password,
+      });
+
+      if (
+        response.status === STATUS_CODE.OK ||
+        response.status === STATUS_CODE.CREATED
+      ) {
+        alert("Usuário criado com sucesso!");
+        navigate("/login");
+        return;
+      }
+
+      alert("Erro no registro. Por favor, tente novamente");
+    } catch (error) {
+      console.error("Erro ao tentar fazer o registro: ", error);
     }
+  }
+
+  return {
+    nameRef,
+    emailRef,
+    passwordRef,
+    confirmPasswordRef,
+    handleRegister,
+  };
 }
